f4f2dbf5d648da0490e25eeb8151dd805119d0c0
André Bisewski <abisewski93@gmail.com>
2022-05-03 21:09:45 -0300

    add script to package json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello Geekhunter!'));
 
-const server = app.listen(process.env.PORT || 5500, () => {
+const server = app.listen(process.env.PORT || 52353, () => {
   const port = server.address().port;
   console.log(`Express is working on port ${port}`);
 });
