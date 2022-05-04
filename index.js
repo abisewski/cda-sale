06714550f70469e706736f4e324c078201fa41e1
André Bisewski <abisewski93@gmail.com>
2022-05-03 21:15:21 -0300

    fetch test

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Geekhunter!'));
+app.get('/', (req, res) => {
+  var headers = {
+    "x-vtex-api-appKey": "vtexappkey-golden-CEWDHC",
+    "x-vtex-api-appToken": "FHGYSNMOHIVBFWZETIEHKCKAVTIXAREZQGQBADRXXQSXGLDZQFYVLOZCXFDZFPOPEVLZGXEQQHJUJMTJPIGVTJOCGNEUZSSICDTLAETTAIEEEFTKMTJWISVUGJSSTJQY"
+  };
+  
+  fetch('/api/catalog_system/pub/products/search?_where=clusterHighlights<210', { headers })
+    .then(res => res.json())
+    .then(data => res.send(data));
+})
 
 const server = app.listen(process.env.PORT || 52353, () => {
   const port = server.address().port;
