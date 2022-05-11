fb54dadbc5154c7a0ff8ab55e4ee40ba747f4c94
André Bisewski <abisewski93@gmail.com>
2022-05-11 20:32:20 -0300

    fix api. add index html

--- index.js
+++ index.js
@@ -1,18 +1,104 @@
 const express = require('express');
 const app = express();
+const axios = require("axios");
 
-app.get('/', (req, res) => {
-  var headers = {
-    "x-vtex-api-appKey": "vtexappkey-golden-CEWDHC",
-    "x-vtex-api-appToken": "FHGYSNMOHIVBFWZETIEHKCKAVTIXAREZQGQBADRXXQSXGLDZQFYVLOZCXFDZFPOPEVLZGXEQQHJUJMTJPIGVTJOCGNEUZSSICDTLAETTAIEEEFTKMTJWISVUGJSSTJQY"
-  };
-  
-  fetch('/api/catalog_system/pub/products/search?_where=clusterHighlights<210', { headers })
-    .then(res => res.json())
-    .then(data => res.send(data));
-})
+app.engine(".html", require("ejs").__express);
 
-const server = app.listen(process.env.PORT || 52353, () => {
+app.set("view engine", "html");
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key =>
+    object[key].includes(value));
+}
+
+
+let date = new Date();
+let sales = {
+  date: '',
+  dateFormated: '',
+  data: []
+};
+
+app.get("/", function (req, res) {
+  res.render("pages/index")
+});
+app.get("/get-sales", function (req, res) {
+  res.setTimeout(2000000);
+  res.send(sales.data);
+});
+
+app.get("/update-sales", function (req, res) {
+  res.setTimeout(2000000);
+
+  console.log('loading');
+
+  getProducts(0)
+
+  function getProducts(currentPagination, rangeTotal = 20000, currentData) {
+    let newData = currentData || [];
+
+    const perPage = 20;
+    const currentPaginationInitial = currentPagination - perPage + 1;
+
+    if (rangeTotal <= currentPaginationInitial) {
+      console.log('send data')
+      sales.data = newData;
+      res.send(newData);
+    } else {
+      console.log('rangeTotal', rangeTotal)
+      console.log('currentPagination', currentPagination)
+
+      var headersAuth = {
+        "x-vtex-api-appKey": "vtexappkey-golden-CEWDHC",
+        "x-vtex-api-appToken": "FHGYSNMOHIVBFWZETIEHKCKAVTIXAREZQGQBADRXXQSXGLDZQFYVLOZCXFDZFPOPEVLZGXEQQHJUJMTJPIGVTJOCGNEUZSSICDTLAETTAIEEEFTKMTJWISVUGJSSTJQY"
+      };
+
+      // axios.get(`https://www.casadasaliancas.com.br/api/catalog_system/pub/products/search/?fq=C:%3a%2f${categoryId}%2f&_from=${currentPaginationInitial}&_to=${currentPagination}&O=OrderByReleaseDateDESC`, { headers: headersAuth })
+      axios.get(`https://www.casadasaliancas.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds?_from=${currentPaginationInitial}&_to=${currentPagination}`, { headers: headersAuth })
+        .then(function (salesResponse) {
+
+          let salesResponseData = salesResponse.data.data;
+          
+          Object.keys(salesResponseData).forEach((productId, index) => {
+            let isSale = false;
+            let lastIndex = Object.keys(salesResponseData).length === (index + 1);
+
+            axios.get(`https://www.casadasaliancas.com.br/api/catalog_system/pub/products/search?fq=productId:${productId}`, { headers: headersAuth })
+              .then(function (pRes) {
+                if (pRes.data[0]) {
+
+                  if (pRes.data[0].items[0]) {
+                    
+                    pRes.data[0].items[0].sellers[0].commertialOffer.DiscountHighLight.forEach(highlight => {
+                      if (getKeyByValue(highlight, 'Oferta') || getKeyByValue(highlight, 'OFF')) {
+                        isSale = true;
+                      }
+                    })
+                  }
+                }
+
+                if(isSale) {
+                  console.log('isSale', productId)
+                  newData.push(pRes.data);
+                }
+
+                if (lastIndex) {
+                  getProducts(currentPagination + perPage, salesResponse.data.range.total, newData);
+                }
+              })
+
+          })
+        })
+        .catch(error => {
+
+          console.log(error)
+          getProducts(currentPagination + perPage, salesResponse.data.range.total, newData);
+        });
+    }
+  }
+});
+
+const server = app.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
   console.log(`Express is working on port ${port}`);
 });
